f5849714f9a4b7cf332a66f2ba1a487e182511ad
Jatin Sharma <37687225+sharmajatin741@users.noreply.github.com>
2019-07-08 17:32:00 +0530

    Update modulopower.cpp

--- modulopower.cpp
+++ modulopower.cpp
@@ -1,1 +1,12 @@
-int mpow(int a,int b,int p=MOD){a=a%p;int res=1;while(b>0){if(b&1)res=(res*a)%p;a=(a*a)%p;b=b>>1;}return res%p;}
+int mpow(int a,int b,int p=MOD)
+{
+  a=a%p;
+  int res=1;
+  while(b>0)
+  {
+    if(b&1) res=(res*a)%p;
+    a=(a*a)%p;
+    b=b>>1;
+  }
+ return res%p;
+}
