118336fb6fe4407a307ab3b1f4c7ea73578664ee
Jatin Sharma <37687225+sharmajatin741@users.noreply.github.com>
2019-07-08 17:39:45 +0530

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+#define fi first
+#define se second
+#define pb push_back
+#define pii pair<int,int>
+#define all(x) (x).begin(), (x).end()
+const int MOD = 1e9 + 7 ;
+const int N=2*1e5+3;
+
+int32_t main(){
+      ios_base::sync_with_stdio(false);
+      cin.tie(NULL);
+      #ifndef ONLINE_JUDGE
+      freopen("input.txt", "r", stdin);
+      // freopen("output.txt","w",stdout);
+      #endif
+ 	  
+}     
+// I never lose. I either win or I learn
