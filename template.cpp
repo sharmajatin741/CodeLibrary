861313473115dec0ad24d06f265919284440b390
Jatin Sharma <37687225+sharmajatin741@users.noreply.github.com>
2020-04-17 22:15:32 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,22 +1,33 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h> 
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
+using namespace __gnu_pbds;
+using namespace chrono;
 #define int long long
-#define ld long double
+#define ld double
 #define fi first
 #define se second
 #define pb push_back
 #define pii pair<int,int>
+#define sz(x) ((long long)x.size())
 #define all(x) (x).begin(), (x).end()
-const int MOD = 1e9 + 7 ;
-const int N=2*1e5+3;
+const int MOD =1e9+7;
+mt19937_64 rng(high_resolution_clock::now().time_since_epoch().count());
+template<typename T> using Tree = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void solve()
+{	
+
+}
 
 int32_t main(){
-      ios_base::sync_with_stdio(false);
-      cin.tie(NULL);
-      #ifndef ONLINE_JUDGE
-      freopen("input.txt", "r", stdin);
-      // freopen("output.txt","w",stdout);
-      #endif
- 	  
-}     
-// I never lose. I either win or I learn
+	ios_base::sync_with_stdio(false);cin.tie(NULL);
+	#ifdef LOCAL
+	freopen("input.txt", "r", stdin);
+	#endif
+	int t=1;
+	// cin>>t;
+	while(t--)
+		solve();
+} 
